6a0f5e849644d74c5d6eb2023cd3355040c5c0f4
l-duong05 <lduong05.git@gmail.com>
2026-05-21 00:01:43 +1000

    added create recipe nav button;

--- client/src/components/search-page/Navbar.tsx
+++ client/src/components/search-page/Navbar.tsx
@@ -6,13 +6,28 @@ import LoginPopup from "./LoginPopup";
 
 export default function Navbar() {
   const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [headerMsg, setHeaderMsg] = useState("");
+  const [bodyMsg, setBodyMsg] = useState("");
+
 
   const loggedIn = Boolean(getAccessToken());
+
+  const handleCreateNav = (event: React.MouseEvent) => {
+    const token = getAccessToken();
+    if (!token) {
+      event?.preventDefault();
+      setHeaderMsg("Log in to create recipes");
+      setBodyMsg("You need to be logged in before you can create new recipes.");
+      setShowLoginPopup(true);
+    }
+  };
 
   const handleSavedRecipeNav = (event: React.MouseEvent) => {
     const token = getAccessToken();
     if (!token) {
       event?.preventDefault();
+      setHeaderMsg("Log in to view saved recipes");
+      setBodyMsg("You need to be logged in before you can view recipes you've saved.");
       setShowLoginPopup(true);
     }
   };
@@ -30,13 +45,21 @@ export default function Navbar() {
       </Link>
 
       <div className="navbar-actions">
+        <Link to="/create-recipe" className="navbar-icon-btn" aria-label="Saved recipes" onClick={(event) => handleCreateNav(event)}>
+          <i className="bi bi-plus-lg" style={{ fontSize: '27px', color: '#fff' }} />
+        </Link>
         <Link to="/saved-recipes" className="navbar-icon-btn" aria-label="Saved recipes" onClick={(event) => handleSavedRecipeNav(event)}>
           <i className="bi bi-bookmark" style={{ fontSize: '23px', color: '#fff' }} />
         </Link>
         {loggedIn ? (
-            <Link to="/" className="navbar-icon-btn" aria-label="Login" onClick={handleLogout}>
-              <i className="bi bi-box-arrow-right" style={{ fontSize: '27px', color: '#fff' }} />
-            </Link>
+            <div className="navbar-profile-actions">
+              <Link to="/profile" className="navbar-icon-btn" aria-label="Profile">
+                <i className="bi bi-person" style={{ fontSize: '27px', color: '#fff' }} />
+              </Link>
+              <Link to="/" className="navbar-icon-btn" aria-label="Login" onClick={handleLogout}>
+                <i className="bi bi-box-arrow-right" style={{ fontSize: '27px', color: '#fff' }} />
+              </Link>
+            </div>
           ) : (
             <Link to="/login" className="navbar-icon-btn" aria-label="Login">
               <i className="bi bi-box-arrow-in-right" style={{ fontSize: '27px', color: '#fff' }} />
@@ -48,8 +71,8 @@ export default function Navbar() {
       {showLoginPopup && 
         <LoginPopup 
           setShowLoginPopup={setShowLoginPopup} 
-          headerMsg={"Log in to view saved recipes"} 
-          bodyMsg={"You need to be logged in before you can view recipes you've saved."} 
+          headerMsg={headerMsg}
+          bodyMsg={bodyMsg} 
         />
       }
     </nav>
